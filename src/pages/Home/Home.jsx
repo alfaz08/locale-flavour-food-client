660a9b4aaaa034dc7300a464d2f8ff96838329e3
Alfaz Hossain <alfaz.cse@gmail.com>
2023-12-05 20:33:20 +0600

    initial setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet-async";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Locale Food | Home</title>
+      </Helmet>
+      
+    </div>
+  );
+};
+
+export default Home;
