146c095eb9be5335f032178c56041142a2e88fe0
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-05 22:48:25 +0600

    payment system basic setup with key in env file

---
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -0,0 +1,69 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+
+const CheckoutForm = () => {
+ 
+ const stripe = useStripe()
+ const elements = useElements()
+ const [error,setError] =useState('')
+
+  const handleSubmit = async(event) =>{
+    event.preventDefault()
+
+    if(!stripe || !elements){
+      return
+    }
+
+    const card =elements.getElement(CardElement)
+
+    if(card===null){
+      return
+    }   
+
+    const {error,paymentMethod} =await stripe.createPaymentMethod({
+      type:'card',
+      card
+    })
+   if(error){
+    console.log('payment method',error);
+    setError(error.message)
+   }
+   else
+   {
+   console.log('payment method',paymentMethod);
+   setError('')
+   }
+    
+  }
+
+
+
+  return (
+   <form onSubmit={handleSubmit}>
+    <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+      <button className="btn btn-warning" type="submit" disabled={!stripe}>
+        Pay
+      </button>
+      <p className="text-red-600">{error}</p>
+   </form>
+  );
+};
+
+export default CheckoutForm;
--- src/components/VendorPayment/Membership.jsx
+++ src/components/VendorPayment/Membership.jsx
@@ -1,9 +1,15 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "../CheckoutForm/CheckoutForm";
 
+const stripePromise =loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK)
 
 const Membership = () => {
   return (
-    <div>
-       Add something
+    <div className="container mx-auto mt-4">
+       <Elements stripe={stripePromise}>
+         <CheckoutForm></CheckoutForm>
+       </Elements>
     </div>
   );
 };
