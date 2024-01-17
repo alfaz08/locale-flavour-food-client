e20424c6c23ac4d3192b8355184f5d7a6f9369be
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-18 01:17:26 +0600

    sweet product is sorted

--- src/pages/ProductsList/Sweet/SweetProducts.jsx
+++ src/pages/ProductsList/Sweet/SweetProducts.jsx
@@ -3,49 +3,94 @@ import { Helmet } from "react-helmet-async";
 import useSweet from "../../../hooks/useSweet";
 import SingleSweetProducts from "./SingleSweetProducts";
 import './sweetitem.css'
+import { useState } from "react";
 const SweetProducts = () => {
   const [allSweet] =useSweet()
   console.log(allSweet);
+  const [sortProduct, setSortProduct] = useState([]);
+  console.log(sortProduct);
+  const handleSort = (event) => {
+    const selectedValue = event.target.value;
+    console.log("Selected value:", selectedValue);
+    const sortedData = allSweet.slice().sort((a, b) => {
+      return selectedValue === "asc"
+        ? a.productPrice - b.productPrice
+        : b.productPrice - a.productPrice;
+    });
+    setSortProduct(sortedData);
+  };
   return (
    
 
     <div className="max-w-screen-2xl mx-auto mt-4">
-    <Helmet>
-         <title>Locale Food | Sweet and Dessert Products</title>
-       </Helmet>
- <div className="hero rounded-xl" style={{backgroundImage: 'url(https://i.ibb.co/6y1pQ2j/header-bg.png)'}}>
-   <div className="hero-overlay bg-opacity-10">
-   <div className="grid justify-start ">
-   <div className=" text-lg  breadcrumbs text-black">
-       <h2 className="text-4xl font-bold p-6">Sweet and Dessert</h2>
-   <ul>
-     <li><a className="text-black p-4">Home</a></li> 
-     <li><a className="text-black ">All Sweet and Dessert Products</a></li> 
-   </ul>
- </div>
-   </div>
+     <Helmet>
+        <title>Locale Food | Fruits Products</title>
+      </Helmet>
+      <div
+        className="hero rounded-xl"
+        style={{
+          backgroundImage: "url(https://i.ibb.co/6y1pQ2j/header-bg.png)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-10">
+          <div className="grid justify-start ">
+            <div className=" text-lg  breadcrumbs text-black">
+              <h2 className="text-4xl font-bold p-6">Fruits </h2>
+              <ul>
+                <li>
+                  <a className="text-black p-4">Home</a>
+                </li>
+                <li>
+                  <a className="text-black ">All Fruits Products</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
 
-   </div>
+      <div className="flex justify-between bg-green-100 rounded-lg">
+        <div>
+          <h2 className="text-xl font-bold mt-4 ml-4">
+            We found {allSweet.length} items for you!
+          </h2>
+        </div>
 
- </div>
+        <div>
+          <div className="flex mr-4 ">
+            <select
+              className="cursor-pointer rounded-md border px-4 py-2 text-center text-gray-600"
+              name="sortBy"
+              id="sortBy"
+              onChange={handleSort}
+            >
+              <option value="">
+                {" "}
 
- <div className="flex justify-between bg-red-300">
-   <div>
-   <h2 className="text-xl font-bold mt-4">We found {allSweet.length} items for you!</h2>
-
-   </div>
- <div>
- <h2 className="text-xl font-bold mt-4">Search</h2>
- </div>
- </div>
-
+                Sort
+              </option>
+              <option value="asc">Price: Low to High</option>
+              <option value="desc">Price: High to Low</option>
+            </select>
+          </div>
+        </div>
+      </div>
 
       {/* card item */}
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-      {
-       allSweet?.map(item=> <SingleSweetProducts key={item._id} item={item}></SingleSweetProducts>
-         )
-      }
+      {sortProduct.length > 0
+          ? sortProduct?.map((item) => (
+              <SingleSweetProducts
+                key={item._id}
+                item={item}
+              ></SingleSweetProducts>
+            ))
+          : allSweet?.map((item) => (
+              <SingleSweetProducts
+                key={item._id}
+                item={item}
+              ></SingleSweetProducts>
+            ))}
       </div>
 
 
