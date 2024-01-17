786d4a532dcd7020990905069e114be65050df0c
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-18 01:19:24 +0600

    vegetables is sorted

--- src/pages/ProductsList/Vegetables/VegetablesProducts.jsx
+++ src/pages/ProductsList/Vegetables/VegetablesProducts.jsx
@@ -2,50 +2,95 @@ import { Helmet } from "react-helmet-async";
 
 import useVegetables from "../../../hooks/useVegetables";
 import SingleVegetablesProducts from "./SingleVegetableProducts";
+import { useState } from "react";
 
 const VegetablesProducts = () => {
   const [allVegetables] =useVegetables()
-  
+  const [sortProduct, setSortProduct] = useState([]);
+  console.log(sortProduct);
+  const handleSort = (event) => {
+    const selectedValue = event.target.value;
+    console.log("Selected value:", selectedValue);
+    const sortedData = allVegetables.slice().sort((a, b) => {
+      return selectedValue === "asc"
+        ? a.productPrice - b.productPrice
+        : b.productPrice - a.productPrice;
+    });
+    setSortProduct(sortedData);
+  };
   return (
     
 
     <div className="max-w-screen-2xl mx-auto mt-4">
     <Helmet>
-         <title>Locale Food | Vegetables Products</title>
-       </Helmet>
- <div className="hero rounded-xl" style={{backgroundImage: 'url(https://i.ibb.co/6y1pQ2j/header-bg.png)'}}>
-   <div className="hero-overlay bg-opacity-10">
-   <div className="grid justify-start ">
-   <div className=" text-lg  breadcrumbs text-black">
-       <h2 className="text-4xl font-bold p-6">Vegetables</h2>
-   <ul>
-     <li><a className="text-black p-4">Home</a></li> 
-     <li><a className="text-black ">All Vegetables Products</a></li> 
-   </ul>
- </div>
-   </div>
+        <title>Locale Food | Fruits Products</title>
+      </Helmet>
+      <div
+        className="hero rounded-xl"
+        style={{
+          backgroundImage: "url(https://i.ibb.co/6y1pQ2j/header-bg.png)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-10">
+          <div className="grid justify-start ">
+            <div className=" text-lg  breadcrumbs text-black">
+              <h2 className="text-4xl font-bold p-6">Fruits </h2>
+              <ul>
+                <li>
+                  <a className="text-black p-4">Home</a>
+                </li>
+                <li>
+                  <a className="text-black ">All Fruits Products</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
 
-   </div>
+      <div className="flex justify-between bg-green-100 rounded-lg">
+        <div>
+          <h2 className="text-xl font-bold mt-4 ml-4">
+            We found {allVegetables.length} items for you!
+          </h2>
+        </div>
 
- </div>
+        <div>
+          <div className="flex mr-4 ">
+            <select
+              className="cursor-pointer rounded-md border px-4 py-2 text-center text-gray-600"
+              name="sortBy"
+              id="sortBy"
+              onChange={handleSort}
+            >
+              <option value="">
+                {" "}
 
- <div className="flex justify-between bg-red-300">
-   <div>
-   <h2 className="text-xl font-bold mt-4">We found {allVegetables.length} items for you!</h2>
-
-   </div>
- <div>
- <h2 className="text-xl font-bold mt-4">Search</h2>
- </div>
- </div>
+                Sort
+              </option>
+              <option value="asc">Price: Low to High</option>
+              <option value="desc">Price: High to Low</option>
+            </select>
+          </div>
+        </div>
+      </div>
 
 
       {/* card item */}
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-      {
-       allVegetables?.map(item=> <SingleVegetablesProducts key={item._id} item={item}></SingleVegetablesProducts>
-         )
-      }
+      {sortProduct.length > 0
+          ? sortProduct?.map((item) => (
+              <SingleVegetablesProducts
+                key={item._id}
+                item={item}
+              ></SingleVegetablesProducts>
+            ))
+          : allVegetables?.map((item) => (
+              <SingleVegetablesProducts
+                key={item._id}
+                item={item}
+              ></SingleVegetablesProducts>
+            ))}
       </div>
 
 
